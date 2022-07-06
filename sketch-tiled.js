0ce6cc61ae8aee55d01178ba8ad1c257e4a07242
Andrew Reitano <andrew.reitano@gmail.com>
2022-07-06 12:44:17 -0400

    json tiles / fixed blit

--- sketch-tiled.js
+++ sketch-tiled.js
@@ -1,8 +1,8 @@
-let _roomTiles;
 let _tileMap;
+let _tiles = [];
 
 let app = new PIXI.Application({
-  width: 512,
+  width: 1024,
   height: 512,
   backgroundColor: 0xffffff,
 });
@@ -10,45 +10,48 @@ document.body.appendChild(app.view);
 app.loader.add(["assets/tiles2x.png"]).load(setup);
 
 function setup() {
-  _roomTiles = new PIXI.tilemap.CompositeRectTileLayer(
-    0,
-    PIXI.utils.TextureCache["assets/tiles2x.png"]
-  );
-  app.stage.addChild(_roomTiles);
   loadMap("assets/maps/exitAction2x.tmj");
 }
 
 async function loadMap(url) {
+  var tileTexture = PIXI.utils.TextureCache["assets/tiles2x.png"];
+
   const response = await fetch(url);
   _tileMap = await response.json();
 
-  app.width = _tileMap.width * _tileMap.tilewidth;
-  app.height = _tileMap.height * _tileMap.tileheight;
+  for (var i = 0; i < _tileMap.layers[0].data.length; i++) {
+    var tileIndex = _tileMap.layers[2].data[i];
+    var tileFlipX = tileIndex && (1 << 32);
+    var tileFlipY = tileIndex && (1 << 31);
+    tileIndex &= 0xFFFF;
 
-  var tileTexture = PIXI.utils.TextureCache["assets/tiles2x.png"];
-  for (var y = 0; y < _tileMap.height; y++) {
-    for (var x = 0; x < _tileMap.width; x++) {
-      var tileNum = _tileMap.layers[0].data[x + _tileMap.width * y];
-      if (tileNum < 2399) {
-        var tileX = parseInt(tileNum % _tileMap.width) * _tileMap.tilewidth;
-        var tileY = parseInt(tileNum / _tileMap.height) * _tileMap.tileheight;
-        tileTexture.frame = new PIXI.Rectangle(
-          tileX,
-          tileY,
-          tileX + _tileMap.tilewidth,
-          tileY + _tileMap.tileheight
-        );
-
-        var rectangle = PIXI.Sprite.from(tileTexture);
-        rectangle.x = x * _tileMap.tilewidth;
-        rectangle.y = y * _tileMap.tileheight;
-        rectangle.width = _tileMap.tilewidth;
-        rectangle.height = _tileMap.tileheight;
-        rectangle.tint = 0xff0000;
-        app.stage.addChild(rectangle);
-
-        console.log(`x: ${x} / y: ${y} / tile ${tileNum}`);
-      }
+    // break out tiled params (bit encoded)
+    if(tileIndex) {
+      tileIndex -= 1;
+      var tileSourceX = parseInt(tileIndex % 10) * _tileMap.tilewidth;
+      var tileSourceY = parseInt(tileIndex / 10) * _tileMap.tileheight;
+      var canvasX = parseInt(i % _tileMap.width) * _tileMap.tilewidth;
+      var canvasY = parseInt(i / _tileMap.width) * _tileMap.tileheight;
+  
+      // texture blit area
+      tileTexture.frame = new PIXI.Rectangle(
+        tileSourceX,
+        tileSourceY,
+        _tileMap.tilewidth,
+        _tileMap.tileheight
+      );
+  
+      var tileSprite = PIXI.Sprite.from(tileTexture);
+      tileSprite.position.x = canvasX;
+      tileSprite.position.y = canvasY;
+      tileSprite.width = _tileMap.tilewidth;
+      tileSprite.height = _tileMap.tileheight;
+      tileSprite.tint = 0xff0000;
+      app.stage.addChild(tileSprite);
+  
+      console.log(`[${i}] ${tileIndex} ${tileSourceX},${tileSourceY}`)
     }
   }
+  // app.width = _tileMap.width * _tileMap.tilewidth;
+  // app.height = _tileMap.height * _tileMap.tileheight;
 }
