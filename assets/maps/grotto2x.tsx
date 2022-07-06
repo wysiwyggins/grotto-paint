e1ef7aeffa09968358a1c43ae5fea04a1a681300
Andrew Reitano <andrew.reitano@gmail.com>
2022-07-06 01:37:04 -0400

    cleanup

---
+++ assets/maps/grotto2x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="tiles2x" tilewidth="40" tileheight="30" tilecount="30" columns="10">
+ <image source="../tiles2x.png" width="400" height="90"/>
+</tileset>
--- sketch-tiled.js
+++ sketch-tiled.js
@@ -1,34 +1,54 @@
-let app = new PIXI.Application(
-  { 
-    width: 256,
-    height: 256,
-    backgroundColor: 0xFFFFFF
-  });
-document.body.appendChild(app.view);
+let _roomTiles;
+let _tileMap;
 
-let _tileset;
-app.loader.add("assets/tileset.png").load(setup);
+let app = new PIXI.Application({
+  width: 512,
+  height: 512,
+  backgroundColor: 0xffffff,
+});
+document.body.appendChild(app.view);
+app.loader.add(["assets/tiles2x.png"]).load(setup);
 
 function setup() {
-  let resources = app.loader.resources;
-  _tileset = new PIXI.Sprite(resources["assets/tileset.png"].texture);
-  app.stage.addChild(_tileset);
+  _roomTiles = new PIXI.tilemap.CompositeRectTileLayer(
+    0,
+    PIXI.utils.TextureCache["assets/tiles2x.png"]
+  );
+  app.stage.addChild(_roomTiles);
+  loadMap("assets/maps/exitAction2x.tmj");
 }
 
-let _tileMap;
 async function loadMap(url) {
   const response = await fetch(url);
   _tileMap = await response.json();
 
-  app.width = _tileMap.width*_tileMap.tileWidth;
-  app.height = _tileMap.height*_tileMap.tileHeight;
+  app.width = _tileMap.width * _tileMap.tilewidth;
+  app.height = _tileMap.height * _tileMap.tileheight;
 
+  var tileTexture = PIXI.utils.TextureCache["assets/tiles2x.png"];
   for (var y = 0; y < _tileMap.height; y++) {
     for (var x = 0; x < _tileMap.width; x++) {
-      console.log(`x: ${x} / y: ${y} / tile ${_tileMap.layers[0].data[x+(x*y)]}`);
+      var tileNum = _tileMap.layers[0].data[x + _tileMap.width * y];
+      if (tileNum < 2399) {
+        var tileX = parseInt(tileNum % _tileMap.width) * _tileMap.tilewidth;
+        var tileY = parseInt(tileNum / _tileMap.height) * _tileMap.tileheight;
+        tileTexture.frame = new PIXI.Rectangle(
+          tileX,
+          tileY,
+          tileX + _tileMap.tilewidth,
+          tileY + _tileMap.tileheight
+        );
+
+        var rectangle = PIXI.Sprite.from(tileTexture);
+        rectangle.x = x * _tileMap.tilewidth;
+        rectangle.y = y * _tileMap.tileheight;
+        rectangle.width = _tileMap.tilewidth;
+        rectangle.height = _tileMap.tileheight;
+        rectangle.tint = 0xff0000;
+        app.stage.addChild(rectangle);
+
+        console.log(`x: ${x} / y: ${y} / tile ${tileNum}`);
+      }
     }
   }
 }
-loadMap("assets/maps/prototype.tmj");
-// console.log(tileMap);
-
