b57e81be47690e28c3f1b6bd6273e25ef7623016
Wiley Wiggins <weevil@wileywiggins.com>
2022-07-22 18:08:46 -0700

    added a tileset of cp437 style glyphs

---
+++ assets/maps/glyphs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="glyphs" tilewidth="40" tileheight="30" tilecount="128" columns="32">
+ <image source="../spritesheets/cp437.png" width="1280" height="120"/>
+</tileset>
