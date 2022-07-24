804cc76b467e842742a8f3a7a10bb747c17529d4
Wiley Wiggins <weevil@wileywiggins.com>
2022-07-23 18:13:07 -0700

    added numbers to glyphs

--- assets/maps/glyphs.tsx
+++ assets/maps/glyphs.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.0" name="glyphs" tilewidth="40" tileheight="30" tilecount="128" columns="32">
- <image source="../spritesheets/cp437.png" width="1280" height="120"/>
+<tileset version="1.9" tiledversion="1.9.0" name="glyphs" tilewidth="40" tileheight="30" tilecount="138" columns="46">
+ <image source="../spritesheets/cp437.png" width="1840" height="90"/>
 </tileset>
