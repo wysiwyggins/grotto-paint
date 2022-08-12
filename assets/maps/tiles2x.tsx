f4d1246896e0d0855a7c1120de655d8f22d5df22
Wiley Wiggins <weevil@wileywiggins.com>
2022-08-11 17:47:57 -0700

    tiled 1.9.1 fixed lots of problems with embedded tilemaps

---
+++ assets/maps/tiles2x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="tiles2x" tilewidth="40" tileheight="30" tilecount="33" columns="11">
+ <image source="../spritesheets/tiles2x.png" width="440" height="90"/>
+</tileset>
