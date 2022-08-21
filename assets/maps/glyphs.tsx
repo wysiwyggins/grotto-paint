354941abd21cd837b369806aef7131d8965469d2
Wiley Wiggins <weevil@wileywiggins.com>
2022-08-21 08:33:52 -0700

    filled in erased spaces with blank tile

---
+++ assets/maps/glyphs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="glyphs" tilewidth="40" tileheight="30" tilecount="138" columns="46">
+ <image source="../spritesheets/cp437.png" width="1840" height="90"/>
+</tileset>
