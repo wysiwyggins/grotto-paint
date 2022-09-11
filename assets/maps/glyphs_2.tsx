96562982c8abc1cb35e9031112b85ef7c1519f6e
Wiley Wiggins <weevil@wileywiggins.com>
2022-09-10 17:03:15 -0700

    bigger glyphs spritesheet

---
+++ assets/maps/glyphs_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="cp437" tilewidth="40" tileheight="30" tilecount="143" columns="13">
+ <image source="../spritesheets/cp437_2.png" width="520" height="330"/>
+</tileset>
