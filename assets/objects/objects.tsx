9bbea6c83dfd08e149975676f64baf913cc1bf21
Wiley Wiggins <weevil@wileywiggins.com>
2022-11-27 12:28:33 -0800

    door objects

--- assets/objects/objects.tsx
+++ assets/objects/objects.tsx
@@ -1,2 +1,109 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="objects" tilewidth="1" tileheight="1" tilecount="0" columns="0"/>
+<tileset version="1.9" tiledversion="1.9.2" name="objects" tilewidth="220" tileheight="150" tilecount="35" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/animal-small.png"/>
+ </tile>
+ <tile id="1">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/arrow-small.png"/>
+ </tile>
+ <tile id="2">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/bird-small.png"/>
+ </tile>
+ <tile id="3">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/brush-small.png"/>
+ </tile>
+ <tile id="4">
+  <image width="120" height="150" source="../../../../Documents/grotto_cp437/objects/building.png"/>
+ </tile>
+ <tile id="5">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/candle-small.png"/>
+ </tile>
+ <tile id="6">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/cenotaph-small.png"/>
+ </tile>
+ <tile id="7">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/door_dark-small.png"/>
+ </tile>
+ <tile id="8">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/door_down-small.png"/>
+ </tile>
+ <tile id="9">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/door_locked-small.png"/>
+ </tile>
+ <tile id="10">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/door_portal-small.png"/>
+ </tile>
+ <tile id="11">
+  <image width="40" height="57" source="../../../../Documents/grotto_cp437/objects/door_trapped-small.png"/>
+ </tile>
+ <tile id="12">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/door_up-small.png"/>
+ </tile>
+ <tile id="13">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/door-small.png"/>
+ </tile>
+ <tile id="14">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/flower-small.png"/>
+ </tile>
+ <tile id="15">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/fungus-small.png"/>
+ </tile>
+ <tile id="16">
+  <image width="40" height="59" source="../../../../Documents/grotto_cp437/objects/ghost-small.png"/>
+ </tile>
+ <tile id="17">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/human-small.png"/>
+ </tile>
+ <tile id="18">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/incense-small.png"/>
+ </tile>
+ <tile id="19">
+  <image width="40" height="51" source="../../../../Documents/grotto_cp437/objects/item_portal-small.png"/>
+ </tile>
+ <tile id="20">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/key-small.png"/>
+ </tile>
+ <tile id="21">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/monk-small.png"/>
+ </tile>
+ <tile id="22">
+  <image width="120" height="120" source="../../../../Documents/grotto_cp437/objects/monster1.png"/>
+ </tile>
+ <tile id="23">
+  <image width="120" height="120" source="../../../../Documents/grotto_cp437/objects/monster2.png"/>
+ </tile>
+ <tile id="24">
+  <image width="120" height="150" source="../../../../Documents/grotto_cp437/objects/monster3.png"/>
+ </tile>
+ <tile id="25">
+  <image width="120" height="120" source="../../../../Documents/grotto_cp437/objects/monster4.png"/>
+ </tile>
+ <tile id="26">
+  <image width="160" height="120" source="../../../../Documents/grotto_cp437/objects/monster5.png"/>
+ </tile>
+ <tile id="27">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/obelisk-small.png"/>
+ </tile>
+ <tile id="28">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/organ-small.png"/>
+ </tile>
+ <tile id="29">
+  <image width="80" height="30" source="../../../../Documents/grotto_cp437/objects/pig.png"/>
+ </tile>
+ <tile id="30">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/robot-small.png"/>
+ </tile>
+ <tile id="31">
+  <image width="220" height="140" source="../../../../Documents/grotto_cp437/objects/saucer.png"/>
+ </tile>
+ <tile id="32">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/vegetable-small.png"/>
+ </tile>
+ <tile id="33">
+  <image width="40" height="60" source="../../../../Documents/grotto_cp437/objects/witch-small.png"/>
+ </tile>
+ <tile id="34">
+  <image width="120" height="88" source="../../../../Documents/grotto_cp437/objects/wumpus.png"/>
+ </tile>
+</tileset>
