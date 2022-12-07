f437d2d9d110170b2c0964bace42b4d5e43df526
Wiley Wiggins <weevil@wileywiggins.com>
2022-12-07 10:50:39 -0800

    renamed assets for scaling

---
+++ assets/spritesheets/grotto-cp437/glyphs@2x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="glyphs" tilewidth="40" tileheight="30" tilecount="143" columns="13">
+ <image source="../cp437_2.png" width="520" height="330"/>
+</tileset>
---
+++ assets/spritesheets/grotto-cp437/tiles@2x.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tiles2x" tilewidth="40" tileheight="30" tilecount="36" columns="12">
+ <image source="../tiles2x.png" width="480" height="90"/>
+ <tile id="19" class="walkable"/>
+</tileset>
